5f2cdf2da044c475213a9247bd846bd44ece976c
Oli <oli@olillin.com>
2025-12-10 23:40:18 +0100

    Update styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,21 +13,23 @@ export default function Page(): React.ReactNode {
             <meta name="description" content="Oli's introduction and portfolio featuring different coding projects and links." />
             <Script src="/scripts/parallax-effect.js" />
 
-            <section className="intro">
-                <img src="/images/olillin.svg" alt="Olillin logo" className="logo" />
-                <h1>Hey, I'm Oli!</h1>
-                <p>I'm a Swedish student of information technology with a passion for code and understanding. I experiment with all sorts of bits and bobs, typically involving a healthy amount of programming.</p>
-                <nav>
-                    <a href="https://github.com/olillin">
-                        <FontAwesomeIcon icon={faGithub} /> GitHub
-                    </a>
-                    <a href="https://youtube.com/@olillin">
-                        <FontAwesomeIcon icon={faYoutube} /> YouTube
-                    </a>
-                </nav>
-            </section>
-            <Projects />
-            <Sites />
+            <main>
+                <section className="intro">
+                    <img src="/images/olillin.svg" alt="Olillin logo" className="logo" />
+                    <h1>Hey, I'm Oli!</h1>
+                    <p>I'm a Swedish student of information technology with a passion for code and understanding. I experiment with all sorts of bits and bobs, typically involving a healthy amount of programming.</p>
+                    <nav>
+                        <a href="https://github.com/olillin">
+                            <FontAwesomeIcon icon={faGithub} /> GitHub
+                        </a>
+                        <a href="https://youtube.com/@olillin">
+                            <FontAwesomeIcon icon={faYoutube} /> YouTube
+                        </a>
+                    </nav>
+                </section>
+                <Projects />
+                <Sites />
+            </main>
         </>
     )
 }
